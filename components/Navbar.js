6ee2767258222ee721a1ffbbb36fb23a79bd9acf
apple <apple@apples-mbp.home>
2019-04-07 20:30:19 +0100

    navbar styled

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,21 +4,12 @@ import Link from 'next/link'
 class Navbar extends Component {
   render () {
     return (
-      <div className='c-navbar'>
-        <input type='checkbox' className='c-navbar__checkbox' id='nav-toggle'/>
-        <label for='nav-toggle' className='c-navbar__button' >
-          <span className='c-navbar__icon'></span>
-        </label>
-        <div className='c-navbar__background'></div>
-        <nav className='c-navbar__nav'>
-          <ul className='c-navbar__list'>
-            <li className='c-navbar__item'><Link href='/index'><a className='c-navbar__link'>Home</a></Link></li>
-            <li className='c-navbar__item'><Link href='/about'><a className='c-navbar__link'>About<a/></a></Link></li>
-            <li className='c-navbar__item'><Link href='/projects'><a className='c-navbar__link'>Projects</a></Link></li>
-            <li className='c-navbar__item'><Link href='/articles'><a className='c-navbar__link'>Articles</a></Link></li>
-          </ul>
-        </nav>
-      </div>
+      <nav className='c-navbar'>
+          <Link href='/index'><a className='c-navbar__link'>Home</a></Link>
+          <Link href='/about'><a className='c-navbar__link'>About</a></Link>
+          <Link href='/projects'><a className='c-navbar__link'>Projects</a></Link>
+          <Link href='/articles'><a className='c-navbar__link'>Articles</a></Link>
+      </nav>
     )
   }
 }
